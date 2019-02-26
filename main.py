b0dde65902c33a6ca5d24097365902801027e8c0
William Lin <wlin26@yahoo.com>
2019-02-26 14:25:46 -0500

    Added comments

--- main.py
+++ main.py
@@ -37,9 +37,7 @@ def main():
                 raise
 
     write_to_audio_file("out/before_smooth.wav", smoothed_signal, sample_rate)
-
     smoothed_signal = smooth_signal(smoothed_signal, 10, 10)
-
     write_to_audio_file("out/after_smooth.wav", smoothed_signal, sample_rate)
 
     threshold = 0.015
@@ -142,6 +140,10 @@ def combine_gender_audio(signals_arr, timestamps, gender_arr):
     return {'clips': all_signals, 'timestamps': new_timestamps, 'genders': new_gender_arr}
 
 def get_cut_times(bool_arr, tolerence=0.2, sr=44100):
+    '''
+    Given a bool array, returns timestamps of chunks where the
+    bool array is low(false) or a given amount of time (tolerence).
+    '''
     time_stamps = list()
     high = False
     i = 0
@@ -170,6 +172,10 @@ def get_cut_times(bool_arr, tolerence=0.2, sr=44100):
     return(time_stamps)
 
 def get_cut_bool_arr(signal, timestamps, sr=44100, padding=0):
+    '''
+    Given timestamps and the original signal array, a boolean array will be created
+    that corresponds the start and stop of where to cut the audio into pieces.
+    '''
     bool_arr = []
     timestamps = (np.array(timestamps) * sr).astype(int).tolist()
     for i, time in enumerate(timestamps):
@@ -192,6 +198,10 @@ def get_cut_bool_arr(signal, timestamps, sr=44100, padding=0):
     return np.array(bool_arr)
     
 def get_silence_bool(arr, threshold, window):
+    '''
+    Returns a boolean array the maps silences that surpass a certain threshold.
+    The boolean array has false for silence and true for audio activity.
+    '''
     bool_arr = list()
     i = 0
     while i < (len(arr) - window):
@@ -204,6 +214,7 @@ def get_silence_bool(arr, threshold, window):
     
     return np.array(bool_arr)
 
+# Given an numpy array, smooth the array using a window and number of passes.
 def smooth_signal(np_arr, window=5, passes=10):
     for i in range(passes):
         weights = np.ones(window) / window
@@ -211,6 +222,7 @@ def smooth_signal(np_arr, window=5, passes=10):
 
     return np_arr
 
+# Writes signal array to a output .wav file.
 def write_to_audio_file(out_path, signal, sample_rate):
     librosa.output.write_wav(out_path, signal, sample_rate)
 
@@ -223,12 +235,14 @@ def split_np_array(signal, bool_arr, padding=0, pad_val=0.):
         splits[i] = np.pad(clip, (padding, padding), 'constant', constant_values=(pad_val,pad_val))
     return splits
 
+# Plots arrays onto a pyplot figure with graphs.
 def plot(*data):
     for i, values in enumerate(data):
         plt.subplot(len(data), 1, i+1)
         plt.plot(values)
     plt.show()
 
+# Uses IBM Watson's speech recognition to get speech to text results
 def getAudioText(filepath):
     try:
         with open(filepath, 'rb') as audio_file:
@@ -242,6 +256,7 @@ def getAudioText(filepath):
     except WatsonApiException as ex:
         print("Method failed with status code " + str(ex.code) + ": " + ex.message)
 
+# TODO::WIP
 def find_shortest_pause(signal):
     '''
     This finds the shortest pause.
